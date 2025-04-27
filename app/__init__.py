96e8303bdbc0f87863d6bf7686a75ef07d730716
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2025-04-27 10:51:43 +0800

    chore: use docker compose (#12)
    Adds docker compose command to build esp binaries.

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask import Flask
 from config import config
 
 
-def create_app(config_name='default'):
+def create_app(config_name="default"):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
