9bdb1ddec056a554ded949f3b7ccfea4fe270292
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2025-05-04 20:50:27 +0800

    chore: add test for esptool stdout (#14)

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ import os
 
 
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'very complicated string'
-    UPLOAD_FOLDER = 'app/static/files/'
-    ALLOWED_EXTENSIONS = ['bin']  # allowed file extensions for uploads
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "very complicated string"
+    UPLOAD_FOLDER = "app/static/files/"
+    ALLOWED_EXTENSIONS = ["bin"]  # allowed file extensions for uploads
     MAX_CONTENT_LENGTH = 1 * 1000 * 1000  # limit max file sizes to 1MB
 
 
@@ -17,6 +17,6 @@ class TestingConfig(Config):
 
 
 config = {
-    'testing': TestingConfig,
-    'default': Config
+    "testing": TestingConfig,
+    "default": Config
 }
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -4,4 +4,4 @@ Confirms that the test app is started in "TESTING" mode.
 
 
 def test_app_is_testing(app):
-    assert app.config['TESTING']
+    assert app.config["TESTING"]
